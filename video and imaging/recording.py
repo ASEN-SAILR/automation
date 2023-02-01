7dd7ac4c0207766842bbc980818590b21984a447
ChrisNylund <cnylund7@gmail.com>
2023-02-01 13:28:33 -0700

    first commit from chris

---
+++ video and imaging/recording.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+
+cap=cv2.VideoCapture(1)
+
+
+out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'),30, (640,480))
+
+while(True):
+
+    ret,frame = cap.read()
+    
+    out.write(frame)
+
+    cv2.imshow('frame',frame)
+    
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):  
+        break 
+
+
+out.release()
+cap.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
