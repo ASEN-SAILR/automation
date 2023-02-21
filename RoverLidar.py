e1f3954cfcc6b09ab86e7592e3f505b6bdc05520
skywalkaa <49798726+skywalkaa@users.noreply.github.com>
2023-02-21 15:51:58 -0700

    RoverLidar getObjects seems to be working

--- RoverLidar.py
+++ RoverLidar.py
@@ -5,7 +5,7 @@ import time
 import logging
 from adafruit_rplidar import RPLidar 
 
-def radialToCart(angl, dist:np.ndarray, type = "rad"):
+def radialToCart(ang, dist:np.ndarray, type = "rad"):
     if type == "deg":
         ang = [val*3.1415/180 for val in ang]
 
@@ -14,21 +14,15 @@ def radialToCart(angl, dist:np.ndarray, type = "rad"):
     return x, y
 
 class RoverLidar:
-    def __init__(self, port:str, start_motor:bool=True, timeout:int=3) -> None:
-        # member vars
-        self.port = port
-        self._lidar = RPLidar(None,port,timeout)
+    def __init__(self,port_name) -> None:
 
-        #map paramaters
-        self.threshold = None
+        self._lidar = RPLidar(None, port_name, timeout=3)
+
         self.x_lim = None
         self.y_lim = None
+        self.threshold = None
         self.red_lim = None
         self.resolution = None
-        self._map_params_set = False 
-
-        if not start_motor: 
-            self.stopMotor() #RPLidar starts motor by default
 
         logging.info("RoverLidar __init__: RoverLidar initialized")
 
@@ -115,9 +109,8 @@ class RoverLidar:
             for (_, angle, distance) in temp_scan:
                 pass
             scan.extend(temp_scan)
-        scan = np.ndarray(scan)
-        logging.debug(f"`measurements` has dimensions of {scan.shape}")
-        return scan
+        
+        return np.array(scan)
 
     def splitScan(self, scan):
         qualitites = scan[:,0]
@@ -128,13 +121,14 @@ class RoverLidar:
 
     def scanToMap(self,scan):
         _, angles, distances = self.splitScan(scan)
+        distances /= 1000
 
-        x_points,y_points = radialToCart(ang=angles, dist=distances)
+        x_points,y_points = radialToCart(ang=angles, dist=distances, type='deg')
         x_mag = self.x_lim[1]-self.x_lim[0]
         y_mag = self.y_lim[1]-self.y_lim[0]
     
         # the following array is a 1D array of bools. True means associated point is within x_lim and y_lim.
-        truth_vals = np.all(np.vstack((x>self.x_lim[0], x<self.x_lim[1], y>self.y_lim[0], y<self.y_lim[1])),0)
+        truth_vals = np.all(np.vstack((x_points>self.x_lim[0], x_points<self.x_lim[1], y_points>self.y_lim[0], y_points<self.y_lim[1])),0)
         
         # removes points outside of x_lim and y_lim
         x_chopped = x_points[truth_vals]
@@ -156,19 +150,19 @@ class RoverLidar:
         # create 2d array for objects 
         x_bins = int(x_mag//self.resolution)
         y_bins = int(y_mag//self.resolution)
-        grid = np.zeros([x_bins,y_bins])
+        object_map = np.zeros([x_bins,y_bins])
 
         # fills the grid with ones where there is an "object". 
         # Looks complicated because `objects` contains negative numbers which cannot be indices
-        grid[objects[:,0]-(int(self.x_lim[0]/self.resolution)),objects[:,1]-(int(self.y_lim[0]/self.resolution))] = 1
+        object_map[objects[:,0]-(int(self.x_lim[0]/self.resolution)+1),objects[:,1]-(int(self.y_lim[0]/self.resolution)+1)] = 1
 
         #get coordiantes of bins in final array
-        x = np.arange(start=self.x_lim[0], stop=self.x_lim[1])
-        y = np.arange(start=self.y_lim[0], stop=self.y_lim[1])
-        xx,yy = np.meshgrid(x,y)
+        x = np.arange(start=self.x_lim[0], stop=self.x_lim[1], step=x_mag/x_bins)
+        y = np.arange(start=self.y_lim[0], stop=self.y_lim[1], step=y_mag/y_bins)
+        xx,yy = np.meshgrid(y,x)
 
-        logging
-        return xx,yy,grid
+        logging.info("finished object map")
+        return xx,yy,object_map,x_binned,y_binned
 
 
     def getMap(self):
@@ -183,22 +177,22 @@ class RoverLidar:
             logging.critical("RoverLidar getMap: Map paramaters not set. Use RoverLidar.setMapParams() to set map paramters.")
             raise Exception("RoverLidar getMap: Map paramaters not set. Use RoverLidar.setMapParams() to set map paramters.")
 
-        if self._lidar.motor_running == False:
-            
-            self.startMotor()
+        # if self._lidar.motor_running == False:   
+        #     self.startMotor()
 
         # start LiDAR laser and collection of data
         #self.startSensing()
 
         # collect data from lidar
-        scan = self.getTimedScan(1) #scans for 1 second 
+        scan = self.getTimedScan(2) #scans for 1 second 
 
         # stop the LiDAR sensing, keep motor running
         # TODO: ensure the LiDAR stops
         #self.stopSensing()
 
         #TODO: turn scans into map
-        xx,yy,grid = self.scanToMap(scan=scan)
+        xx,yy,grid,x_binned,y_binned = self.scanToMap(scan=scan)
+        
         return xx,yy,grid
 
 
@@ -213,6 +207,32 @@ class RoverLidar:
             color: color of zone where there are obstacles
             obstacles: coordiantes of obstacles located in map[(x1,y1),(x2,y2),...,(xn,yn)]  
         """
-        color = None
-        obstacles = None
-        return color,obstacles
+        scan = self.getTimedScan(2)
+
+        _, angles, distances = self.splitScan(scan)
+        distances /= 1000
+
+        x_points,y_points = radialToCart(ang=angles, dist=distances, type='deg')
+        x_mag = self.x_lim[1]-self.x_lim[0]
+        y_mag = self.y_lim[1]-self.y_lim[0]
+    
+        # the following array is a 1D array of bools. True means associated point is within x_lim and y_lim.
+        truth_vals = np.all(np.vstack((x_points>self.x_lim[0], x_points<self.x_lim[1], y_points>self.y_lim[0], y_points<self.y_lim[1])),0)
+        
+        # removes points outside of x_lim and y_lim
+        x_chopped = x_points[truth_vals]
+        y_chopped = y_points[truth_vals]
+        
+
+        # puts hit counts into integer coordnitates 
+        # ex: x,y = (0.1,0.2) would be binned into (1,2) if the resolution was 0.1
+        x_binned = [val//self.resolution for val in x_chopped]
+        y_binned = [val//self.resolution for val in y_chopped]
+
+        # this returns unique points (defined by coords in x_binned and y_binned) and their counts
+        coords,counts = np.unique(list(zip(x_binned,y_binned)),return_counts=True,axis=0)
+
+        # objects are points where there are more hits than the threshold 
+        objects = np.array(coords[counts>self.threshold], dtype=np.int_)
+
+        return objects[:,0]*self.resolution,objects[:,1]*self.resolution,x_chopped,y_chopped
