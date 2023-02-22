63f0a0a427ee1b6a26eef677dfcd1e8872b19570
ChrisNylund <cnylund7@gmail.com>
2023-02-22 14:54:28 -0700

    stuff

--- video and imaging/stitching.py
+++ video and imaging/stitching.py
@@ -2,20 +2,29 @@
 import cv2
 import numpy as np
 
-cam_port0=0
-cam_port1=1
-cam_port2=2
-result0, frame0 = cv2.VideoCapture(cam_port0)
-result1, frame1 = cv2.VideoCapture(cam_port1)
-result2, frame2 = cv2.VideoCapture(cam_port2)
+#port = list of camera ID from /dev
+
+cam0 = cv2.VideoCapture()
+cam0.open(0)
+
+cam1 = cv2.VideoCapture()
+cam1.open(1)
+
+cam2 = cv2.VideoCapture()
+cam2.open(1)
+
+# result2, frame2 = cv2.VideoCapture("USB\VID_0C45&PID_6366&MI_00"
+result0,frame0 = cam0.read()
+result1,frame1 = cam1.read()
+result2,frame2 = cam2.read()
 
 image_paths=[frame0,frame1,frame2]
-
+print(type(frame0))
 # initialized a list of images
 imgs = []
  
 for i in range(len(image_paths)):
-    imgs.append(cv2.imread(image_paths[i]))
+    imgs.append(image_paths[i])
     imgs[i]=cv2.resize(imgs[i],(0,0),fx=0.2,fy=0.2)
     # this is optional if your input images isn't too large
     # you don't need to scale down the image
--- video and imaging/testphoto.py
+++ video and imaging/testphoto.py
@@ -1,8 +1,10 @@
 import numpy as np
 import cv2
 
-capture = cv2.VideoCapture(0)
+capture = cv2.VideoCapture(-1)
 result,frame=capture.read()
+	
+cv2.resize(frame, (1920,1080))
 
 cv2.imwrite('testphoto.jpg',frame)
 
