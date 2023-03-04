b86bd8727f203cd3cd2d942d1f5f150944dd48db
Suphakan <susu8167@colorado.edu>
2023-03-03 23:12:35 -0700

    combine readFromTeensy and teensyMessageAvailable, and add complete sendTranslate and sendStop

--- RoverUART.py
+++ RoverUART.py
@@ -14,7 +14,7 @@ class RoverUART:
         """
         TODO: send stop command to teensy
         """
-        cmdString = "we need a standard for this"
+        cmdString = "s"
         self.sendUartCmd(cmdString)
 
     def sendRotateCmd(self,rad) -> bool:
@@ -24,11 +24,11 @@ class RoverUART:
         cmdString = "r" + struct.pack("<f",rad)
         self.sendUartCmd(cmdString)
 
-    def sendTranslateCmd(self,rotation):
+    def sendTranslateCmd(self,meter):
         """
         TODO: sends teensy a command to translate over UART connection
         """
-        cmdString = "we need a standard for this"
+        cmdString = "t" + struct.pack("<f",meter)
         self.sendUartCmd(cmdString)
         
 
@@ -38,11 +38,16 @@ class RoverUART:
         """
         self.ser.write(cmd)
 
+''' # combine this with the old "teensyAvailable" function below
     def readFromTeensy(self):
         # TODO: reads message from teensy
-        pass
+        if self.teensyMsgAvail():
+            return self.ser.readline()
+        else:
+            return 
+'''
 
-    def teensyMsgAvail(self):
+    def readFromTeensy(self):
         if self.ser.in_waiting>0:
-            return True
+            return self.ser.readline()
         return False
