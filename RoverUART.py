edbdf76d4b60dff8273070ff645397af9930aeb4
skywalkaa <49798726+skywalkaa@users.noreply.github.com>
2023-03-07 09:29:07 -0700

    added functions for getting magnetic heading

--- RoverUART.py
+++ RoverUART.py
@@ -1,20 +1,18 @@
 # Luke
 import struct
 import serial
+import time
+
 class RoverUART:
     def __init__(self,teensy_port) -> None:
         #member vars
         self.ser = serial.Serial(teensy_port, 115200, timeout=1)
-    
-        #initialize
-
-        
 
     def sendStopCmd(self):
         """
-        TODO: send stop command to teensy
+        send stop command to teensy
         """
-        cmdString = "s"
+        cmdString = "s" + struct.pack("<f",0.0)
         self.sendUartCmd(cmdString)
 
     def sendRotateCmd(self,rad) -> bool:
@@ -26,28 +24,42 @@ class RoverUART:
 
     def sendTranslateCmd(self,meter):
         """
-        TODO: sends teensy a command to translate over UART connection
+        sends teensy a command to translate over UART connection
         """
         cmdString = "t" + struct.pack("<f",meter)
         self.sendUartCmd(cmdString)
-        
+
+    def getMagneticAzm(self):
+        """
+        gets the azimuth to magnetic north
+        """
+        cmdString = "m" + struct.pack("<f",0.0)
+        self.sendUartCmd(cmdString)
+        time.sleep(0.25)
+        string = self.readLine()
+        if string[0] != 'm':
+            return -999
+        return float(string[1:-1])
+
+
 
     def sendUartCmd(self,cmd):
         """
-        sends the cmd specified by cmd
+        sends the command specified by cmd
         """
         self.ser.write(cmd)
+ 
+    def msgsWaiting(self):
+        """
+        returns number lines available to read from teensy
+        """
+        return self.ser.in_waiting
 
-''' # combine this with the old "teensyAvailable" function below
-    def readFromTeensy(self):
-        # TODO: reads message from teensy
-        if self.teensyMsgAvail():
-            return self.ser.readline()
-        else:
-            return 
-'''
 
-    def readFromTeensy(self):
+    def readLine(self):
+        """
+        read from serial buffer until a newline is reached
+        """
         if self.ser.in_waiting>0:
             return self.ser.readline()
-        return False
+        return -1
