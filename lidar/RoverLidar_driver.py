c2d1a57b69bd258b580fe14f0ec25ce4ad7a83aa
skywalkaa <49798726+skywalkaa@users.noreply.github.com>
2023-03-09 13:29:51 -0700

    better usability

--- lidar/RoverLidar_driver.py
+++ lidar/RoverLidar_driver.py
@@ -1,11 +1,14 @@
 # import sys
 import matplotlib.pyplot as plt
+import numpy as np
  
 # setting path
 # sys.path.append('../.')
 
 from RoverLidar import RoverLidar
 import logging
+
+DATA_NAME = "lidar_test_20200222_"
 
 if __name__ == "__main__":
     logging.info("begin main")
@@ -18,21 +21,43 @@ if __name__ == "__main__":
     lidar_port = "COM9"
     lidar = RoverLidar(port_name=lidar_port)
     logging.info("about to set mapParams")
+    x_lim=np.array((0,3)) 
+    y_lim=np.array((-2,2))
+    threshold=0
+    red_lim=np.array((0.5,0.5))
+    resolution=0.1
     lidar.setMapParams(
-                    x_lim=(0,3), 
-                    y_lim=(-1,1), 
-                    threshold=5, 
-                    red_lim=(0.5,0.5), 
-                    resolution=0.05
+                    x_lim=x_lim, 
+                    y_lim=y_lim, 
+                    threshold=threshold, 
+                    red_lim=red_lim, 
+                    resolution=resolution
                     )
+    # path = r"C:\Users\luker\OneDrive - UCB-O365\Classes\2023_spring\ASEN 4028\Teams Shortcuts\General\JPL Senior Projects\Testing\LiDAR Testing Material\data\TRR_scan_15_spray_paint.npy"
+    # scan = np.load(path)
+    objects,x_chopped,y_chopped = lidar.getObstacles(2)
 
-    x,y,x_chopped,y_chopped = lidar.getObstacles()
-    
-    fig,ax = plt.subplots()
-    ax.scatter(x,y)
 
     fig,ax = plt.subplots()
     ax.scatter(x_chopped,y_chopped)
+    ax.set_title(DATA_NAME+"hits")
+    ax.set_xlabel("x [m]")
+    ax.set_ylabel("y [m]")
+    ax.set_xlim(x_lim*1.1)
+    ax.set_ylim(y_lim*1.1)
+    ax.set_aspect("equal")
+    ax.grid()
+
+    fig,ax = plt.subplots()
+    ax.scatter(objects[:,0],objects[:,1])
+    ax.set_title(DATA_NAME+"Obstacles")
+    ax.set_xlabel("x [m]")
+    ax.set_ylabel("y [m]")
+    ax.set_xlim(x_lim*1.1)
+    ax.set_ylim(y_lim*1.1)
+    ax.set_aspect("equal")
+    ax.grid()
+
     plt.show()
 
     
