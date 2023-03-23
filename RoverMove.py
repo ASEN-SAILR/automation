d0b0445e22c4d0bdbeaf49a51258453c8aefd4fb
Suphakan <susu8167@colorado.edu>
2023-03-23 15:03:14 -0600

    add logging in autonomous

--- RoverMove.py
+++ RoverMove.py
@@ -130,6 +130,7 @@ class RoverMove:
 
 					#Checks if at LOI
 					atloi = self.gps.atloi(LOI)
+                logging.info('[lat,lon]='+str(gps.getGPS())+', dist='+str(gps.distanceToTarget(LOI))+' m, angle from N='+str(gps.bearingToTarget(LOI))+' deg, MagHeading='+str(mag_heading)+' deg, atloi='+str(atloi))
 
 	def checkDesiredHeading(self,delta_heading):
 		'''
